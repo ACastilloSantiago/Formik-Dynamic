0757d4e6c3acf19de4a56d73cb42baed257dabf5
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-08-30 10:22:32 -0300

    feat: :sparkles: add a logic for validations

--- src/03-forms/pages/DynamicFormPage.tsx
+++ src/03-forms/pages/DynamicFormPage.tsx
@@ -1,13 +1,38 @@
 import { Formik, Form } from "formik";
+import * as Yup from 'yup'
 
 import formJson from "../data/custom-form.json";
-import { MyTextInput } from "../components";
+import { MySelect, MyTextInput } from "../components";
 
 
 const initialValues: { [key: string]: any } = {};
+const requiredFields: { [key: string]: any } = {};
 for (const input of formJson) {
     initialValues[input.name] = input.value
+
+    if (!input.validations) continue
+
+    let schema = Yup.string()
+
+    for (const rule of input.validations) {
+
+        if (rule.type === 'required') {
+            schema = schema.required('Este campo es requerido.')
+        }
+        if (rule.type === 'minLength') {
+            schema = schema.min((rule as any).value || 2, `Mínimo de ${(rule as any).value || 2} caracteres.`)
+        }
+        if (rule.type === 'email') {
+            schema = schema.email('Revise el formato del email.')
+        }
+
+        // ... otras reglas.
+
+    }
+    requiredFields[input.name] = schema
 }
+
+const validationSchema = Yup.object({ ...requiredFields })
 
 export const DynamicFormPage = () => {
     return (
@@ -15,23 +40,50 @@ export const DynamicFormPage = () => {
             <h1>
                 DynamicFormPage
             </h1>
+
             <Formik
                 initialValues={initialValues}
                 onSubmit={(values) => console.log(values)}
+                validationSchema={validationSchema}
             >
                 {(formik) => (
                     <Form noValidate>
                         {
-                            formJson.map(({ label, name, placeholder, type }) => {
-                                return (
+                            formJson.map(({ label, name, placeholder, type, options }) => {
 
-                                    <MyTextInput
-                                        key={name}
-                                        label={label}
-                                        name={name}
-                                        type={type as any}
-                                        placeholder={placeholder} />
-                                )
+                                if (type === 'input' || type === 'email' || type === 'password') {
+
+                                    return (
+
+                                        <MyTextInput
+                                            key={name}
+                                            label={label}
+                                            name={name}
+                                            type={type as any}
+                                            placeholder={placeholder} />
+                                    )
+                                } else if (type === 'select') {
+
+                                    return (
+
+                                        <MySelect key={name} label={label} name={name} >
+
+                                            <option value=''>Select an option</option>
+                                            {
+                                                options?.map(({ id, label }) => (
+
+                                                    <option key={id} value={label}>{label}</option>
+
+                                                ))
+                                            }
+
+                                        </MySelect>
+
+                                    )
+
+                                }
+
+                                return <span>Type: {type}</span>
                             })
                         }
                         <button type="submit">Submit</button>
